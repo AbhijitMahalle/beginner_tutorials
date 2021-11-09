468ab52820b69e08f59f2dbfc7a8a8e89f8a9951
abhi-mah <69358122+abhi-mah@users.noreply.github.com>
2021-11-08 23:35:03 -0500

    added addTwoNums.cpp

--- src/modify_str_svc_node.cpp
+++ src/modify_str_svc_node.cpp
@@ -1,26 +1,34 @@
 /**
- * @file change_str_svc_node.cpp
+ * @file ros_publisher.cpp
  * @author Abhijit Mahalle
- * @brief service node to run the service modify the input string
+ * @brief Class for addTwoNums.cpp
  * @version 0.1
- * @date 2021-11-07
- * 
+ * @date 2021-11-01
  * @copyright Copyright (c) 2021
  * 
  */
 
-#include <beginner_tutorials/modify_string_service.hpp>
-#include <memory>
+#include <ros/ros.h>
+#include <beginner_tutorials/AddTwoInts.h>
+
+bool add(beginner_tutorials::AddTwoInts::Request &req,  // NOLINT-CPP
+         beginner_tutorials::AddTwoInts::Response &res) {  // NOLINT-CPP
+    res.sum = req.a + req.b;
+    ROS_INFO_STREAM("request: num1="
+                    << req.a
+                    << "num2="
+                    << req.b);
+    ROS_INFO_STREAM("sending back response: " << res.sum);
+    return true;
+}
 
 int main(int argc, char **argv) {
-  ros::init(argc, argv, "modify_string_service");
-  ROS_INFO_STREAM("Initialized /modify_string_service node.");
+    ros::init(argc, argv, "add_two_ints_server");
+    ros::NodeHandle n;
 
-  ros::NodeHandle ros_node_h;
-  std::unique_ptr<ChangeStringService> modify_str_svc(
-      new ChangeStringService(ros_node_h,
-                              "modify_string_service"));
-  ROS_INFO_STREAM("Turning up /modify_string_service.");
-  modify_str_svc->turn_up_service();
-  return 0;
+    ros::ServiceServer service = n.advertiseService("add_two_ints", add);
+    ROS_INFO("Ready to add two ints.");
+    ros::spin();
+
+    return 0;
 }
