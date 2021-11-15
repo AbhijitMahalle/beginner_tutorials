1bb7b39addd7cbd279fdc7ff3d794a93f7bcd384
Abhijit Mahalle <abhijeetmahalle27@gmail.com>
2021-11-15 10:46:13 -0500

    Added license

--- include/ros_publisher.hpp
+++ include/ros_publisher.hpp
@@ -1,4 +1,26 @@
 /**
+* MIT License
+*
+* Copyright(c) 2021 Abhijit Mahalle
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files(the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+* 
 * @file ros_publisher.hpp
 * @author Abhijit Mahalle
 * @brief Class to publish message 
@@ -9,13 +31,16 @@
 * 
 */
 
-#ifndef SRC_BEGINNER_TUTORIALS_INCLUDE_BEGINNER_TUTORIALS_ROS_PUBLISHER_HPP_
-#define SRC_BEGINNER_TUTORIALS_INCLUDE_BEGINNER_TUTORIALS_ROS_PUBLISHER_HPP_
+#ifndef SRC_BEGINNER_TUTORIALS_INCLUDE_BEGINNER_TUTORIALS_ROS_PUBLISHER_HPP_ //  NOLINT-CPP
+#define SRC_BEGINNER_TUTORIALS_INCLUDE_BEGINNER_TUTORIALS_ROS_PUBLISHER_HPP_ //  NOLINT-CPP
 
 #include <ros/ros.h>
 #include <std_msgs/String.h>
 #include <sstream>
 #include <string>
+#include <memory>
+
+#include <include/tfbroadcaster.hpp>
 
 /**
  * @brief Class to publish message
@@ -51,13 +76,16 @@ class ROSPublisher {
   ros::ServiceClient add_svc_client;
 
   /**
-   * @brief method to call the service
+   * Pointer to the ROSTfBroadcaster object to broadcast poses
    * 
-   * @param a 
-   * @param b 
-   * @return std::string modified string to be published
    */
-  std::string call_service(int a, int b);
+  std::shared_ptr<TfBroadcaster> tf_broadcast;
+
+   /**
+   * @brief Method to broadcast a static frame transform
+   * 
+   */
+  void broadcast_transformation();
 };
 
-#endif  // SRC_BEGINNER_TUTORIALS_INCLUDE_BEGINNER_TUTORIALS_ROS_PUBLISHER_HPP_
+#endif  // SRC_BEGINNER_TUTORIALS_INCLUDE_BEGINNER_TUTORIALS_ROS_PUBLISHER_HPP_//  NOLINT-CPP
